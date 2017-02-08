9d6f328a4c2eec8d45e3abcb18fa6467abc57d0b
Tiki <Tiki@DESKTOP-7SCSCAC>
2017-02-08 20:24:16 +0200

    Helo

---
+++ Teh02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            amplifier amplifier = new amplifier();
+            bool result;
+            do
+            {
+                Console.Write("Give a new volume value (0-100): ");
+                string line = Console.ReadLine();
+                int number;
+                result = int.TryParse(line, out number);
+                amplifier.Amp = number;
+                Console.WriteLine("Amplifier volume is now se to: " + amplifier.Amp);
+            } while (result == true);
+
+        }
+    }
+}
---
+++ Teh02/amplifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht02
+{
+    class amplifier
+    {
+        private int minamp = 0;
+        private int maxamp = 100;
+        private int amp = 0;
+
+        public int Amp
+        {
+            get
+            {
+                return amp;
+            }
+            set
+            {
+                if (value >= minamp && value <= maxamp)
+                {
+                    amp = value;
+                }
+                else if (value < 0)
+                {
+                    amp = minamp;
+                    Console.WriteLine("Too low volume. Volume is set to minimum: 0");
+                }
+                else if (value > 100)
+                {
+                    amp = maxamp;
+                    Console.WriteLine("Too much volume. Volume is set to maximum: 100");
+                }
+            }
+        }
+    }
+}
