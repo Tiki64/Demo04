241aacd6b8a32ef0a614eafc66a2ced8fbbc9e80
Tiki <Tiki@DESKTOP-7SCSCAC>
2017-02-09 00:17:37 +0200

    Radio

---
+++ Teh05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teh05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Radio radio = new Radio();
+                radio.Power(true);
+                radio.SetFreq(2000.8);
+                radio.SetVolume(5);
+                Console.WriteLine(radio.ToString());
+            }
+        }
+    }
+}
---
+++ Teh05/Radio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teh05
+    {
+        class Radio
+        {
+            public bool PowerOn { get; set; }
+            public int Volume { get; set; }
+            public double Freq { get; set; }
+
+            public void Power(bool poweron)
+            {
+                PowerOn = poweron;
+            }
+
+            public void SetFreq(double freq)
+            {
+                if (freq <= 26000 && freq >= 2000)
+                {
+                    Freq = freq;
+                }
+            }
+
+            public void SetVolume(int volume)
+            {
+                if (volume >= 1 && volume <= 9)
+                {
+                    Volume = volume;
+                }
+            }
+
+            public override string ToString()
+            {
+                return "Power: " + PowerOn + "\nVolume: " + Volume + "\nFrequency: " + Freq;
+            }
+        }
+    }
